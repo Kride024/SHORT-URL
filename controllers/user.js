088c22ba8841579bfb93b79d6715b65d5f367372
Kride024 <kritiyadavcoding@gmail.com>
2025-08-17 20:33:21 +0530

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,8 @@
 const User = require("../models/user");
+const { v4: uuidv4 } = require('uuid');
+const {setUser} = require('../service/auth');
+
+
 
 async function handleUserSignup(req,res){
     const {name,email,password} = req.body;
@@ -27,6 +31,11 @@ async function handleUserLogin(req,res){
             error:"Invalid email or Password"
         })
     }
+
+    const sessionId = uuidv4();
+    setUser(sessionId,user);
+    res.cookie("uid", sessionId);
+    
     return res.redirect("/");
 }
 module.exports= {
